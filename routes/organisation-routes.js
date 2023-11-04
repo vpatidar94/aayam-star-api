1135d56b66b30780344690e666fb1f74672b289c
Tushar Kumrawat <125907891+Tushar411@users.noreply.github.com>
2023-11-04 20:02:34 +0530

    [added]: new sub-admin role

--- routes/organisation-routes.js
+++ routes/organisation-routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const { verifyToken, verifyAdminToken, verifySuperAdminToken, } = require("../middleware/jwt-token");
+const { verifySuperAdminToken, verifySuperAdminAndSubAdminToken, } = require("../middleware/jwt-token");
 const router = express.Router();
 const { addOrganisation, getOrganisations, updateOrganisation, getOrganisationById, sendLoginMessage } = require("../controller.js/organisation-controller")
 
 // protected routes
 router.post("/addOrganisation", verifySuperAdminToken, addOrganisation);
-router.get("/", verifySuperAdminToken, getOrganisations);
-router.put("/updateOrganisation/:orgId", verifySuperAdminToken, updateOrganisation)
-router.get("/getOrganisation/:orgId", verifySuperAdminToken, getOrganisationById)
-router.post("/sendLoginMessage", verifySuperAdminToken, sendLoginMessage);
+router.get("/", verifySuperAdminAndSubAdminToken, getOrganisations);
+router.put("/updateOrganisation/:orgId", verifySuperAdminAndSubAdminToken, updateOrganisation)
+router.get("/getOrganisation/:orgId", verifySuperAdminAndSubAdminToken, getOrganisationById)
+router.post("/sendLoginMessage", verifySuperAdminAndSubAdminToken, sendLoginMessage);
 
 module.exports = router;
